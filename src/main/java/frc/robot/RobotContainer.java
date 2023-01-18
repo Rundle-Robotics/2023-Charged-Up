95309ca4bdfe343affa76d0319f5b14fc279f946
Ishan Vermani <38046444+ishanvermani@users.noreply.github.com>
2023-01-17 20:42:41 -0500

    Comments on Errors

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -25,7 +25,22 @@ public class RobotContainer {
 			OperatorConstants.DRIVER_CONTROLLER_PORT);
 
 		
-	}
+	/*
+	 * The bracket below is causing problems
+	 * How can I tell? 
+	 * 1) All the errors start directly below that line
+	 * 2) Clicking on it highlights the RobotContainer bracket in line 22
+	 * 3) the color matchs the bracket in line 22, and there are no brackets between it could be closing off
+	 * 
+	 * The RobotContainer bracket should encompass all of the code! After all, it is the class we are going to initialize in Robot.java
+	 * 
+	 * Therefore, this bracket should be deleted. Doing so removes all the errors (aside from the second set of comments below)
+	 * 
+	 * If you ever have bracket errors, look at where the errors start, the color, and the paried bracket when clicked on
+	 */
+
+
+	}	// THIS BRACKET SHOULD NOT BE HERE AAAHHHH
 	// The robot's subsystems and commands are defined here...
 	private final Drivetrain drivetrain = new Drivetrain(driverController);
 
@@ -36,8 +51,31 @@ public class RobotContainer {
 	public RobotContainer() {
 		// Configure the trigger bindings
 		configureBindings();
+
+
+		/*
+		 * You haven't defined the kYButton variable. What button do we want? We want the Y button of the controller
+		 * Do we have the controller? Yes - driverController!
+		 * 
+		 * Now, you imported the CommandXboxController class, not the XboxController class. The CommandXboxController lets you tie commands to buttons. 
+		 * If you just want to change a variable (finetuned) to true, we only want to check the button status, not tie it to a command
+		 * 
+		 * To check status, we can't use a CommandXboxController object, we need an ordinary XboxController object.
+		 * 
+		 * Looking at the documentation (https://first.wpi.edu/wpilib/allwpilib/docs/release/java/edu/wpi/first/wpilibj2/command/button/CommandXboxController.html),
+		 * the .getHID() command returns the XboxController that this is based off
+		 * 
+		 * so doing driverController.getHID() gives us an XboxController object we can poll the status for
+		 * 
+		 * to get the Y button of an XboxController Object? Check the documentation. It's .getYButton()
+		 * 
+		 * Therefore, in this case, as you don't want to tie an explicit command to the Y button, and just poll its status,
+		 * we can do driverController.getHID().getYButton()
+		 * 
+		 * we can also avoid this mess by changing the object type of driverController to XboxController instead of CommandXboxController.
+		 */
+
 		if (kYButton = true) {
-		
 		boolean finetuned = true;
 
 		}
