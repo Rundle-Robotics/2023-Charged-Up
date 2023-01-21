41c6a07881a7fc36ab6bb1375f36da886cf443e6
DevenVElani <118859678+DevenVElani@users.noreply.github.com>
2023-01-21 13:54:12 -0700

    rea

--- src/main/java/frc/robot/subsystems/SparkMax.java
+++ src/main/java/frc/robot/subsystems/SparkMax.java
@@ -19,11 +19,24 @@ import com.revrobotics.SparkMaxPIDController;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.SparkMaxRelativeEncoder;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 public class SparkMax {
+    private static final int I = 0;
     public CANSparkMax m;
     public RelativeEncoder e;
+
     public SparkMax() {
         m = new CANSparkMax(0,MotorType.kBrushless);
         e = m.getEncoder();
+        while ( I == 0) {
+            public double v = e.getVelocity();
+            public double p = e.getPosition();
+            double CPR = e.getCountsPerRevolution();
+        }   
     }
 }
+@Override
+	public void periodic() {            
+        SmartDashboard.putNumber("Velocity", v);
+
+    }
