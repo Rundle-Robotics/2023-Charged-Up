5757185b32339f0023b5d040f8c51133b9fd1434
Rundle Shared ThinkPad <rundleroboticsgithub@gmail.com>
2023-02-04 11:30:16 -0700

    updTE

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -37,7 +37,7 @@ public class RobotContainer {
 	 */
 	public RobotContainer() {
 
-		compressor = new Compressor(0, PneumaticsModuleType.REVPH);
+		compressor = new Compressor(1, PneumaticsModuleType.REVPH);
         compressor.enableDigital();
 		// Configure the trigger bindings
 		
