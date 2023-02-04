7ca8c397c5dfad6ead566ea1b85f1842c3fe720f
DevenVElani <118859678+DevenVElani@users.noreply.github.com>
2023-02-04 14:30:02 -0700

    cleaned
    clean little subsystem

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -11,8 +11,6 @@ import frc.robot.subsystems.GrabberLifter;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.GrabberLowerCommand;
 /**
  * This class is where the bulk of the robot should be declared. Since
--- src/main/java/frc/robot/subsystems/GrabberLifter.java
+++ src/main/java/frc/robot/subsystems/GrabberLifter.java
@@ -1,19 +1,7 @@
 package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
-import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import frc.robot.Constants.ControlConstants;
-import frc.robot.Constants.OperatorConstants;
-
-
-import frc.robot.subsystems.Drivetrain;
-import frc.robot.RobotContainer;
-
-import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import com.revrobotics.RelativeEncoder;
 public class GrabberLifter extends SubsystemBase {
@@ -36,7 +24,7 @@ public class GrabberLifter extends SubsystemBase {
         else if (lifted == false && lowered == false && p >= 180 && p <= 220 || lifted == false && lowered == false && p >= 380 && p <= 420 || lifted == false && lowered == false && p >= 580 && p <= 620 || lifted == false && lowered == false && p >= 780 && p <= 820 ) {
             m.set(0);
         }
-        else if (lowered == false && lifted == false){
+        else if (lowered == false && lifted == true){
             m.set(-2);
         }
         else {
