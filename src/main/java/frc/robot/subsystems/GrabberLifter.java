df2789bb7429fb8c35c1e8b43b34050ea786dae2
DevenVElani <118859678+DevenVElani@users.noreply.github.com>
2023-02-07 17:09:24 -0700

    Final(Final)

--- src/main/java/frc/robot/subsystems/GrabberLifter.java
+++ src/main/java/frc/robot/subsystems/GrabberLifter.java
@@ -2,18 +2,14 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import com.revrobotics.CANSparkMaxLowLevel;
-
 import com.revrobotics.RelativeEncoder;
-import com.revrobotics.CANSparkMax.IdleMode;
+
 public class GrabberLifter extends SubsystemBase {
     private CANSparkMax m;
     private RelativeEncoder e;
-    private double lifted;
     public GrabberLifter() {
         m = new CANSparkMax(4, MotorType.kBrushless);
         e = m.getEncoder();
-        lifted = 0;
         m.setIdleMode(CANSparkMax.IdleMode.kBrake);
     }
     
