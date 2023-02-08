2775fa1b15f893084be7a08cdd56fe648e18c3c7
Damian Suidgeest <damiansuidgeest@gmail.com>
2023-02-07 17:08:37 -0800

    Change toggling over to command bound to A button

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,79 +8,32 @@ import frc.robot.Constants.OperatorConstants;
 import frc.robot.subsystems.Pneumatics;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 
-/**
- * This class is where the bulk of the robot should be declared. Since
- * Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in
- * the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of
- * the robot (including
- * subsystems, commands, and trigger mappings) should be declared here.
- */
 public class RobotContainer {
-	// Replace with CommandPS4Controller or CommandJoystick if needed
 	public static final CommandXboxController driverController = new CommandXboxController(
 			OperatorConstants.DRIVER_CONTROLLER_PORT);
 
-	// The robot's subsystems and commands are defined here...
+	// Subsystems
+	private static Compressor compressor;
 	private final Pneumatics pneumatics;
 
-
-	public static Compressor compressor;
-
-	/**
-	 * The container for the robot. Contains subsystems, OI devices, and commands.
-	 */
 	public RobotContainer() {
-
 		compressor = new Compressor(PneumaticsModuleType.REVPH);
         compressor.enableDigital();
 
 		pneumatics = new Pneumatics();
-
 		
 		// Configure the trigger bindings
-		
 		configureBindings();
-
-		
 	}
 
-	/**
-	 * Use this method to define your trigger->command mappings. Triggers can be
-	 * created via the
-	 * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with
-	 * an arbitrary
-	 * predicate, or via the named factories in {@link
-	 * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for
-	 * {@link
-	 * CommandXboxController
-	 * Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-	 * PS4} controllers or
-	 * {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-	 * joysticks}.
-	 */
 	private void configureBindings() {
-		// Example: Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-		// new Trigger(drivetrain::exampleCondition)
-		// .onTrue(new ExampleCommand(m_exampleSubsystem));
-
-		// Example: Schedule `exampleMethodCommand` when the Xbox controller's B button
-		// is
-		// pressed,
-		// cancelling on release.
-		// m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+		// Bind A button to climber solenoid (toggle)
+		driverController.a().onTrue(pneumatics.toggleClimberSolenoid());
 	}
 
-	/**
-	 * Use this to pass the autonomous command to the main {@link Robot} class.
-	 *
-	 * @return the command to run in autonomous
-	 */
 	public Command getAutonomousCommand() {
 		return null;
 	}
--- src/main/java/frc/robot/subsystems/pneumatics.java
+++ src/main/java/frc/robot/subsystems/pneumatics.java
@@ -3,36 +3,30 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.RobotContainer;
 
 public class Pneumatics extends SubsystemBase {
-    private DoubleSolenoid sol1;
-    /** Creates a new Climber. */
-    public Pneumatics() {
-  
-      // set solenoid values (placeholder values)
-      sol1 = new DoubleSolenoid(PneumaticsModuleType.REVPH, 0, 1); 
-      sol1.set(Value.kReverse);
-  
-    }
-    @Override
-    public void periodic() {
-      // This method will be called once per scheduler run
-      boolean enabled = RobotContainer.compressor.isEnabled();
-      boolean pressureSwitch = RobotContainer.compressor.getPressureSwitchValue();
-      double current = RobotContainer.compressor.getCurrent();
-      System.out.println(enabled);
-      System.out.print(pressureSwitch);
-      System.out.print(current);
-      if (RobotContainer.driverController.getHID().getAButtonPressed()) {
-        // if you press the b button, then go up
-        sol1.toggle();
-        System.out.println("A button pressed");
-      }
-    }
-    public void setClimber(Value value) {
-      sol1.set(value);
-      
-    }
-  }
+	private DoubleSolenoid sol1;
+
+	public Pneumatics() {
+		// set solenoid values (placeholder values)
+		sol1 = new DoubleSolenoid(PneumaticsModuleType.REVPH, 0, 1); 
+		sol1.set(Value.kReverse);
+	}
+
+	@Override
+	public void periodic() {
+	}
+
+	public void setClimber(Value value) {
+		sol1.set(value);
+	}
+
+	public CommandBase toggleClimberSolenoid() {
+		return runOnce(() -> {
+			sol1.toggle();
+			System.out.println("Toggling climber solenoid"); // debug
+		});
+	}
+}
