e58ccb5818d26a5f3722edee7f731bc802cde665
Ishan Vermani <38046444+ishanvermani@users.noreply.github.com>
2023-02-14 19:04:11 -0500

    Added Switch logic

--- src/main/java/frc/robot/subsystems/GrabberLifter.java
+++ src/main/java/frc/robot/subsystems/GrabberLifter.java
@@ -1,6 +1,8 @@
 package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.RelativeEncoder;
 
@@ -8,6 +10,11 @@ public class GrabberLifter extends SubsystemBase {
     private CANSparkMax rm;
     private CANSparkMax lm;
     private RelativeEncoder e;
+
+    private DigitalInput bottomSwitch;
+    private DigitalInput middleSwitch;
+    private DigitalInput topSwitch;
+
     public GrabberLifter() {
         rm = new CANSparkMax(5, MotorType.kBrushless);
         lm = new CANSparkMax(7, MotorType.kBrushless);
@@ -15,6 +22,10 @@ public class GrabberLifter extends SubsystemBase {
         e = rm.getEncoder();
         rm.setIdleMode(CANSparkMax.IdleMode.kBrake);
         lm.setIdleMode(CANSparkMax.IdleMode.kBrake);
+
+        bottomSwitch = new DigitalInput(0);
+        middleSwitch = new DigitalInput(1);
+        topSwitch = new DigitalInput(2);
     }
     
     public double getPosOfLift() {
@@ -24,6 +35,29 @@ public class GrabberLifter extends SubsystemBase {
         rm.set(newValue);
         lm.set(newValue);
     }
+
+    @Override
+    public void periodic() {
+
+        double speed = rm.get();
+        if (getTopSwitch() && speed > 0)
+        {
+            lift(0);
+        } 
+        else if (getMiddleSwitch() && speed < 0)
+        {
+            lift(0);
+        }
+        else if (!getBottomSwitch() && getMiddleSwitch())
+        {
+            lift(0);
+        }
+    }
+
+
+    public boolean getTopSwitch() {return topSwitch.get(); }
+    public boolean getMiddleSwitch() {return middleSwitch.get(); }
+    public boolean getBottomSwitch() {return bottomSwitch.get(); }
     
 }
 
