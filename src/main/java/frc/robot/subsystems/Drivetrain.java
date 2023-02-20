cf3b06e85f6b8c4d5132ddccef5d5f2ff4ccd486
Ishan Vermani <38046444+ishanvermani@users.noreply.github.com>
2023-02-20 09:01:13 -1000

    Inverted controls and added Ramp Rate

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -51,6 +51,12 @@ public class Drivetrain extends SubsystemBase {
 		backLeft.setIdleMode(CANSparkMax.IdleMode.kBrake);
 		backRight.setIdleMode(CANSparkMax.IdleMode.kBrake);
 
+		//set robot to acceleate from 0 to max speed in a MINIMUM of 2 seconds
+		frontLeft.setOpenLoopRampRate(2);
+		backLeft.setOpenLoopRampRate(2);
+		frontRight.setOpenLoopRampRate(2);
+		backRight.setOpenLoopRampRate(2);
+
 
 	}
 
@@ -59,8 +65,8 @@ public class Drivetrain extends SubsystemBase {
 	@Override
 	public void periodic() {
 
-		double joyX = RobotContainer.driverController.getRawAxis(OperatorConstants.XBOX_LEFT_X_AXIS);
-		double joyY = -RobotContainer.driverController.getRawAxis(OperatorConstants.XBOX_LEFT_Y_AXIS);
+		double joyX = -RobotContainer.driverController.getRawAxis(OperatorConstants.XBOX_LEFT_X_AXIS);
+		double joyY = RobotContainer.driverController.getRawAxis(OperatorConstants.XBOX_LEFT_Y_AXIS);
 		double rotation = ControlConstants.ROTATION_MULT
 				* (RobotContainer.driverController.getRawAxis(OperatorConstants.XBOX_RIGHT_X_AXIS));
 		mecanumDrive(joyX, joyY, rotation);
