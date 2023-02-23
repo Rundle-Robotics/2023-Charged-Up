4113d2a66a93f4f05054b741348781b03f284401
andrew Cook <cooka@rundlecollege.ca>
2023-02-22 17:03:28 -0700

    lower to posiiotn
    hilp

--- src/main/java/frc/robot/commands/LowerToPosition.java
+++ src/main/java/frc/robot/commands/LowerToPosition.java
@@ -4,11 +4,38 @@
 
 package frc.robot.commands;
 
+
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.GrabberLifter;
+import frc.robot.subsystems.Pneumatics;
+
+
 
 public class LowerToPosition extends CommandBase {
   /** Creates a new LowerToPosition. */
-  public LowerToPosition() {
+
+  
+
+
+
+private boolean fin;
+
+ 
+
+private GrabberLifter grabberLifter;
+private Pneumatics pneumatics;
+private double speed;
+
+  public LowerToPosition(GrabberLifter grabberLifter, Pneumatics pneumatics) {
+    this.grabberLifter = grabberLifter;
+    this.pneumatics = pneumatics;
+        speed = 0;
+        
+        fin = false;
+        addRequirements(grabberLifter);
+        addRequirements(pneumatics);
+
+    
     // Use addRequirements() here to declare subsystem dependencies.
   }
 
@@ -18,15 +45,35 @@ public class LowerToPosition extends CommandBase {
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+
+    if (grabberLifter.getBottomSwitch() != true){
+      speed = -0.2;
+      grabberLifter.lift(speed);
+    }
+   
+
+    
+    
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+
+    if (interrupted != true){
+      pneumatics.toggleLifter();
+      fin = true;
+    }
+  }
+
+  
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return fin;
   }
 }
+
+
