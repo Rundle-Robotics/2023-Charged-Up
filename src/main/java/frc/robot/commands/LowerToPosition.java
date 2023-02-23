d86cee27efc691e30d18ef12ecb3885c18f39908
Damian Suidgeest <damiansuidgeest@gmail.com>
2023-02-22 16:06:45 -0800

    Formatting

--- src/main/java/frc/robot/commands/LowerToPosition.java
+++ src/main/java/frc/robot/commands/LowerToPosition.java
@@ -4,76 +4,46 @@
 
 package frc.robot.commands;
 
-
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.GrabberLifter;
 import frc.robot.subsystems.Pneumatics;
 
-
-
 public class LowerToPosition extends CommandBase {
-  /** Creates a new LowerToPosition. */
+	/** Creates a new LowerToPosition. */
 
-  
+	private GrabberLifter grabberLifter;
+	private Pneumatics pneumatics;
 
+	public LowerToPosition(GrabberLifter grabberLifter, Pneumatics pneumatics) {
+		this.grabberLifter = grabberLifter;
+		this.pneumatics = pneumatics;
 
+		addRequirements(grabberLifter);
+		addRequirements(pneumatics);
+	}
 
-private boolean fin;
+	// Called when the command is initially scheduled.
+	@Override
+	public void initialize() {
+	}
 
- 
+	// Called every time the scheduler runs while the command is scheduled.
+	@Override
+	public void execute() {
+		grabberLifter.lift(-0.2);
+	}
 
-private GrabberLifter grabberLifter;
-private Pneumatics pneumatics;
-private double speed;
+	// Called once the command ends or is interrupted.
+	@Override
+	public void end(boolean interrupted) {
+		if (!interrupted) {
+			pneumatics.toggleLifter();
+		}
+	}
 
-  public LowerToPosition(GrabberLifter grabberLifter, Pneumatics pneumatics) {
-    this.grabberLifter = grabberLifter;
-    this.pneumatics = pneumatics;
-        speed = 0;
-        
-        fin = false;
-        addRequirements(grabberLifter);
-        addRequirements(pneumatics);
-
-    
-    // Use addRequirements() here to declare subsystem dependencies.
-  }
-
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {}
-
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-
-    if (grabberLifter.getBottomSwitch() != true){
-      speed = -0.2;
-      grabberLifter.lift(speed);
-    }
-   
-
-    
-    
-  }
-
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {
-
-    if (interrupted != true){
-      pneumatics.toggleLifter();
-      fin = true;
-    }
-  }
-
-  
-
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return fin;
-  }
+	// Returns true when the command should end.
+	@Override
+	public boolean isFinished() {
+		return grabberLifter.getBottomSwitch();
+	}
 }
-
-
