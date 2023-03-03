ed2194ab985ed21d2781f1ba58851d39f7f5699c
Damian Suidgeest <damiansuidgeest@gmail.com>
2023-03-03 00:16:20 -0800

    Undo overwrite AutoBalancePossibility, overwrite NewAutoBalance instead

--- src/main/java/frc/robot/commands/AutoBalancePossibility.java
+++ src/main/java/frc/robot/commands/AutoBalancePossibility.java
@@ -1,7 +1,12 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot.commands;
 
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.NAVX;
+import frc.robot.auto.BackupAutoMove;
 import edu.wpi.first.math.controller.PIDController;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
@@ -10,19 +15,36 @@ public class AutoBalancePossibility extends CommandBase {
 
 	private Drivetrain drivetrain;
 	private NAVX navx;
+	private boolean finished;
+	private boolean tilted;
 
-	private PIDController backUpOnStationPID;
+  private double distance;
+  private Drivetrain drive;
 
-	private static final double DISTANCE = 20;
-	private static final double MAX_SPEED = 0.3;
-	private static final double ROLL_DEADBAND = 9;
+  private double initialPosition;
+  private double currentPosition;
+  private double targetPosition;
 
-	private boolean backingUp;
+  private BackupAutoMove auto;
+
+  private double initialRoll;
+  
+  private boolean finite;
+
+  private PIDController yPID;
+  private int mu;
+  private static final double MAX_SPEED = 0.3;
+  private boolean tracking;
+  private double roll;
+  private double setpoint;
 
 	/** Creates a new AutoBalanceNAvX. */
-	public AutoBalancePossibility(Drivetrain drivetrain, NAVX navx) {
+	public AutoBalancePossibility(Drivetrain drivetrain, NAVX navx, BackupAutoMove auto) {
+		// Use addRequirements() here to declare subsystem dependencies.
+
 		this.drivetrain = drivetrain;
 		this.navx = navx;
+    this.auto = auto;
 
 		addRequirements(drivetrain);
 		addRequirements(navx);
@@ -31,34 +53,75 @@ public class AutoBalancePossibility extends CommandBase {
 	// Called when the command is initially scheduled.
 	@Override
 	public void initialize() {
-		backUpOnStationPID = new PIDController(0.05, 0, 0);
-		backUpOnStationPID.setTolerance(2); // 2 ticks (1/21 of a rotation)
-		backUpOnStationPID.setSetpoint(DISTANCE);
+    roll = navx.getRoll();
+    tracking = false;
+		initialPosition = drive.getFrontLeftPosition();
 
-		backingUp = true;
+    initialRoll = navx.getRoll();
+
+    targetPosition = initialPosition + (distance);
+
+    yPID = new PIDController(0.05,0, 0);
+
+    yPID.setTolerance(2); //2 ticks (1/21 of a rotation)
+
+    yPID.setSetpoint(targetPosition);
+
+    finite = false;
+
+
+    
 	}
 
+  //once roll changes, drive x meters. if roll is not == 0 then drive another 1 m, and check, and again again again. 
+
+	// Called every time the scheduler runs while the command is scheduled.
 	@Override
 	public void execute() {
-		// Check if we hit the charge station (and should stop backing up)
-		if (Math.abs(navx.getRoll()) > ROLL_DEADBAND) {
-			backingUp = false;
-		}
+		currentPosition = navx.getRoll();
+   
 
-		if (backingUp) {
-			drivetrain.mecanumDrive(0, 0.3, 0);
-		} else if (!backUpOnStationPID.atSetpoint()) {
-			double currentPosition = (drivetrain.getFrontLeftPosition() + drivetrain.getFrontRightPosition()) / 2;
-			double output = backUpOnStationPID.calculate(currentPosition);
+    double output = yPID.calculate(currentPosition);
 
-			if (output > MAX_SPEED) {
-				output = MAX_SPEED;
-			} else if (output < -MAX_SPEED) {
-				output = -MAX_SPEED;
-			}
+    if (output > MAX_SPEED) {
+      output = MAX_SPEED;
+    }
+    else if (output < -MAX_SPEED) {
+      output = -MAX_SPEED;
+    }
+   //Damian look at this code too, possibly finish the code lower too, not finished atm., this 1 is a placeholder value. ishan wants a lock on the lower code. 
+    // if (navx.getRoll()>9) {
+      
+    //   new BackupAutoMove(1, drive);
+    //   if (navx.getRoll()<9){
+    //     new BackupAutoMove(1, drive);
+    //     if (navx.getRoll()<9){
+    //       new BackupAutoMove(1, drive);
+    //       if (navx.getRoll()<9){
+    //         new BackupAutoMove(1, drive);
+    //         if (navx.getRoll()<9){
+    //           new BackupAutoMove(1, drive);
+    //         }}}else {
+    //           finite = true;
+    //         }
+    //   }
+      
+   // }
 
-			drivetrain.mecanumDrive(0, output, 0);
-		}
+   if (Math.abs(roll) > 9) {
+    setpoint = currentPosition + 5;
+    output = -.25;
+    tracking = true;
+   } else {
+    output = 0;
+    finite = true;
+  }
+
+
+
+    drive.mecanumDrive(0, output, 0);
+
+		
 	}
 
 	// Called once the command ends or is interrupted.
@@ -70,40 +133,9 @@ public class AutoBalancePossibility extends CommandBase {
 	// Returns true when the command should end.
 	@Override
 	public boolean isFinished() {
-		return backUpOnStationPID.atSetpoint();
+		return finite;
 	}
 }
 
-// Andrew's old execute code:
-// Damian look at this code too, possibly finish the code lower too, not
-// finished atm., this 1 is a placeholder value. ishan wants a lock on the lower
-// code.
-// Andrew what is this
-// if (navx.getRoll()>9) {
-//
-// new BackupAutoMove(1, drive);
-// if (navx.getRoll()<9){
-// new BackupAutoMove(1, drive);
-// if (navx.getRoll()<9){
-// new BackupAutoMove(1, drive);
-// if (navx.getRoll()<9){
-// new BackupAutoMove(1, drive);
-// if (navx.getRoll()<9){
-// new BackupAutoMove(1, drive);
-// }}}else {
-// finite = true;
-// }
-// }
+  
 
-// }
-
-// if (Math.abs(roll) > 9) {
-// setpoint = currentPosition + 5;
-// output = -.25;
-// tracking = true;
-// } else {
-// output = 0;
-// finite = true;
-// }
-
-// drive.mecanumDrive(0, output, 0);
--- src/main/java/frc/robot/commands/NewAutoBalance.java
+++ src/main/java/frc/robot/commands/NewAutoBalance.java
@@ -1,47 +1,74 @@
 package frc.robot.commands;
 
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.NAVX;
+import edu.wpi.first.math.controller.PIDController;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
 
 public class NewAutoBalance extends CommandBase {
 
     private Drivetrain drivetrain;
     private NAVX navx;
-    private PIDController pid;
 
-    private static final double STOPPED_MOVING_THRESHOLD = 3; // 3 is more stopped than 5
+    private PIDController backUpOnStationPID;
 
-    // Use roll
+    private static final double DISTANCE = 20;
+    private static final double MAX_SPEED = 0.3;
+    private static final double ROLL_DEADBAND = 9;
+
+    private boolean backingUp;
+
     public NewAutoBalance(Drivetrain drivetrain, NAVX navx) {
         this.drivetrain = drivetrain;
         this.navx = navx;
-        pid = new PIDController(0.2, 0, 0);
-        pid.setSetpoint(0);
-        pid.setTolerance(10); // TODO
+
+        addRequirements(drivetrain);
+        addRequirements(navx);
+    }
+
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+        backUpOnStationPID = new PIDController(0.05, 0, 0);
+        backUpOnStationPID.setTolerance(2); // 2 ticks (1/21 of a rotation)
+        backUpOnStationPID.setSetpoint(DISTANCE);
+
+        backingUp = true;
     }
 
     @Override
     public void execute() {
-        double speed = pid.calculate(navx.getRoll());
-        if (Math.abs(speed) > 0.15) {
-            speed = speed / Math.abs(speed) * 0.25;
+        // Check if we hit the charge station (and should stop backing up)
+        if (Math.abs(navx.getRoll()) > ROLL_DEADBAND) {
+            backingUp = false;
         }
-        drivetrain.mecanumDrive(0, speed, 0);
+
+        if (backingUp) {
+            drivetrain.mecanumDrive(0, 0.3, 0);
+        } else if (!backUpOnStationPID.atSetpoint()) {
+            double currentPosition = (drivetrain.getFrontLeftPosition() + drivetrain.getFrontRightPosition()) / 2;
+            double output = backUpOnStationPID.calculate(currentPosition);
+
+            if (output > MAX_SPEED) {
+                output = MAX_SPEED;
+            } else if (output < -MAX_SPEED) {
+                output = -MAX_SPEED;
+            }
+
+            drivetrain.mecanumDrive(0, output, 0);
+        }
     }
 
+    // Called once the command ends or is interrupted.
     @Override
     public void end(boolean interrupted) {
         drivetrain.mecanumDrive(0, 0, 0);
-        System.out.println("NewAutoBalance finished"); // debug
     }
 
+    // Returns true when the command should end.
     @Override
     public boolean isFinished() {
-        boolean leftStopped = Math.abs(drivetrain.getBackLeftVelocity()) <= STOPPED_MOVING_THRESHOLD;
-        boolean rightStopped = Math.abs(drivetrain.getBackRightVelocity()) <= STOPPED_MOVING_THRESHOLD;
-        return pid.atSetpoint() && leftStopped && rightStopped;
+        return backUpOnStationPID.atSetpoint();
     }
-
 }
